6bd332a824187d4a729391c7ff4c7c3f7d65ba77
Sophie <zsofiliszkai@gmail.com>
2025-01-06 18:36:49 +0100

    $ and # in @Value
    $ and # in @Value

---
+++ ValueAnnotation.java
@@ -0,0 +1,32 @@
+package com.spring.professional.exam.tutorial.module01.question34.beans;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class ValueAnnotation {
+
+	//application.property: app.file.property=appFileProperty
+    @Value("${app.file.property}")
+    private String appFileProperty;
+    
+    //environmental variables
+    @Value("${JAVA_HOME}")
+    private String javaHome;
+    @Value("${OS}")
+    private String osName;
+
+    //SPEL
+    @Value("#{2 + 2}")
+    private int additionResult;
+    @Value("#{'New York'.toUpperCase()}")
+    private String cityName;
+    //class.static method
+    @Value("#{T(java.time.LocalDateTime).now()}")
+    private LocalDateTime currentDate;
+    @Value("#{systemEnvironment['JAVA_HOME']}")
+    private String javaHomeSpel;
+
+}
