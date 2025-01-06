fa7a385320eb6e767b16aa054bb525e10bc1d6f8
Sophie <zsofiliszkai@gmail.com>
2025-01-06 19:10:07 +0100

    Update SpelExpressions.java

--- spel/beans/SpelExpressions.java
+++ spel/beans/SpelExpressions.java
@@ -1,13 +1,15 @@
+package com.spring.professional.exam.tutorial.module01.question33.beans;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpelExpressions {
 
-    @Value("#{T(com.spring.professional.exam.tutorial.module01.question33.beans.SpringBean2).staticMethod()}")
+    @Value("#{T(com....beans.SpringBean2).staticMethod()}")
     private String staticMethodExecutionResult;
 
-    @Value("#{T(com.spring.professional.exam.tutorial.module01.question33.beans.SpringBean2).STATIC_VALUE}")
+    @Value("#{T(com....beans.SpringBean2).STATIC_VALUE}")
     private String staticValueFetchResult;
 
     @Value("#{@springBean2.property}")
@@ -25,31 +27,7 @@ public class SpelExpressions {
     @Value("#{systemEnvironment['JAVA_HOME']}")
     private String javaHome;
 
-    public String getStaticMethodExecutionResult() {
-        return staticMethodExecutionResult;
-    }
-
-    public String getStaticValueFetchResult() {
-        return staticValueFetchResult;
-    }
-
-    public String getPropertyValue() {
-        return propertyValue;
-    }
-
-    public String getMethodValue() {
-        return methodValue;
-    }
-
-    public String getAppFileProperty() {
-        return appFileProperty;
-    }
-
-    public String getAppVmProperty() {
-        return appVmProperty;
-    }
-
-    public String getJavaHome() {
-        return javaHome;
-    }
+    
 }
+
+
